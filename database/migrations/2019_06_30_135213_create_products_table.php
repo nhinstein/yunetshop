ee234a65fbbf69e1c5bb6c6a1374b612107124ac
Sri Wahyuni <nhinstein@gmail.com>
2019-07-18 19:34:58 +0700

    Initial commit untuk project yunetshop

---
+++ database/migrations/2019_06_30_135213_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('description');
+            $table->decimal('weight', 6, 2);
+            $table->decimal('price', 8, 0);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
